c42e9868997e5b1944ab507797089b40ae15d338
Angel Pérez Poot <perezpootangelgabriel@gmail.com>
2024-10-11 15:59:29 -0600

    Direccion nosotros

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importa Link de react-router-dom
 
 const Navbar = () => {
   return (
     <header className="bg-[#4b3621] p-4">
       <nav className="flex justify-between items-center">
         <ul className="flex space-x-4">
-          <li><a href="#" className="text-white">Nosotros</a></li>
+          <li><a href="/nosotros" className="text-white">Nosotros</a></li>
           <li><a href="#" className="text-white">Contáctanos</a></li>
-          <li><a href="#" className="text-white">Personajes</a></li>
+          <li><a href="#" className="text-white">Mascotas</a></li>
           <li><a href="#" className="text-white">Regístrate</a></li>
           <li><a href="#" className="text-white">Cuenta</a></li>
-          <li><a href="#" className="text-white">Creadores</a></li>
+          <li><Link to="/" className="text-white">Inicio</Link></li>
         </ul>
         <div className="text-white text-xl font-bold">LOGO</div>
       </nav>
