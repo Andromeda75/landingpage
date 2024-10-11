bcf1f5b57cdd4d8255c87b3a4e349bfc23f1db16
Angel Pérez Poot <perezpootangelgabriel@gmail.com>
2024-10-11 15:58:12 -0600

    Ruta nosotros

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,33 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importa Router y Routes
 import Hero from './components/Hero';
 import Mascotas from './components/Mascotas';
+import Nosotros from './components/Nosotros'; // Ajusta la ruta según sea necesario
+
 import Footer from './components/Footer';
 import './diseño.css';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showNosotros, setShowNosotros] = useState(false);
+
+  const handleNavigation = () => {
+    setShowNosotros(true);
+  };
 
   return (
-    <div>
-    <Navbar />
-    <Hero />
-    <Mascotas />
-    <Footer />
-  </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<><Hero /><Mascotas /></>} />
+          <Route path="/nosotros" element={<Nosotros />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
